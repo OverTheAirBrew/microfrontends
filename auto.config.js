c7d69834639da24a3fae8f612d2864a653658221
Nick Sharp <nick.sharp.87@gmail.com>
2023-05-19 17:15:58 +0100

    feat: update config

--- auto.config.js
+++ auto.config.js
@@ -2,17 +2,17 @@ module.exports = {
   baseBranch: 'main',
   // TODO: Investigate if the order is important
   plugins: [
-    'npm',
-    'conventional-commits',
-    // 'first-time-contributor',
-    // 'all-contributors',
-    'released',
     [
       'protected-branch',
       {
         requiredStatusChecks: ['Build'],
       },
     ],
+    'npm',
+    'conventional-commits',
+    // 'first-time-contributor',
+    // 'all-contributors',
+    'released',
     // 'microsoft-teams', // uses env MICROSOFT_TEAMS_WEBHOOK_URL
   ],
   prereleaseBranches: [],
